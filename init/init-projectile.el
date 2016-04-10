b8e34b667182ec6c0fcca75b1ba0c9c5cf75a3fd
Mario Fernandez <mario.fernandez@hceris.com>
2016-04-10 13:54:45 +0200

    Fix loading settings

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -1,7 +1,10 @@
 (with-eval-after-load 'projectile
- (setq projectile-enable-caching t)
- (add-to-list-multiple 'projectile-globally-ignored-files '("gems.tags" "tags"))
- (add-to-list-multiple 'helm-grep-ignored-directories '("tmp" "coverage"))
- )
+  (setq projectile-enable-caching t)
+  )
+
+(with-eval-after-load 'helm
+  (add-to-list-multiple 'projectile-globally-ignored-files '("gems.tags" "tags"))
+  (add-to-list-multiple 'helm-grep-ignored-directories '("tmp" "coverage"))
+  )
 
 (provide 'init-projectile)
