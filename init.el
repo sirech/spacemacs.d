352ed247797843e193bed655c1b10e010fe55d4b
Mario Fernandez <mario.fernandez@xing.com>
2016-04-21 16:09:20 +0200

    Seems desktop is slowing everything down

--- init.el
+++ init.el
@@ -260,9 +260,6 @@ you should place you code here."
 
   (setq powerline-default-separator nil)
 
-  (desktop-save-mode 1)
-  (desktop-read)
-
   ;; require is only loaded once, so this breaks if put there
   (when (spacemacs/system-is-mac)
     (setq mac-command-modifier 'meta)
