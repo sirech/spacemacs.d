be00e0f8ac0cc3e2fc1dd20acb31a1db4848947d
Mario Fernandez <mario.fernandez@xing.com>
2017-01-29 17:47:42 +0100

    More fine grained undo

--- init.el
+++ init.el
@@ -279,6 +279,7 @@ you should place you code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-want-fine-undo t)
  '(js-indent-level 2)
  '(js2-mode-show-parse-errors nil)
  '(js2-mode-show-strict-warnings nil)
