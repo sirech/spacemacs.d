e7a33644cf54867bf605f17fea1979d1babad9e4
Mario Fernandez <mario.fernandez@xing.com>
2017-07-19 21:34:00 +0200

    Adapt code from react layer to use in custom one

--- layers/aj-javascript/packages.el
+++ layers/aj-javascript/packages.el
@@ -1,12 +1,17 @@
 (defconst aj-javascript-packages
   '(
     add-node-modules-path
+    company
     company-flow
+    ;; company-tern
     eslintd-fix
+    evil-matchit
     flycheck
     prettier-js
-    rjsx-mode))
-
+    rjsx-mode
+    smartparens
+    ;; tern
+    ))
 
 (defun aj-javascript/init-eslintd-fix ()
   (use-package eslintd-fix
@@ -38,31 +43,6 @@
     :config
     (modify-syntax-entry ?_ "w" js2-mode-syntax-table)))
 
-(defun aj-javascript/post-init-add-node-modules-path ()
-  (with-eval-after-load 'rjsx-mode
-    (add-hook 'rjsx-mode-hook #'add-node-modules-path)))
-
-(defun aj-javascript/post-init-company-flow ()
-  (spacemacs|add-company-backends
-    :backends
-    '((company-flow :with company-dabbrev-code)
-      company-files)))
-
-(defun temporary/enable-flycheck (mode)
-  "Use flycheck in MODE by default, if `syntax-checking-enable-by-default' is
-true."
-  (when (and syntax-checking-enable-by-default
-             (listp flycheck-global-modes)
-             (not (eq 'not (car flycheck-global-modes))))
-    (add-to-list 'flycheck-global-modes mode)))
-
-(defun aj-javascript/post-init-flycheck ()
-  (with-eval-after-load 'flycheck
-    (push 'javascript-jshint flycheck-disabled-checkers)
-    (push 'json-jsonlint flycheck-disabled-checkers))
-
-  (temporary/enable-flycheck 'rjsx-mode))
-
 (defun aj-javascript/init-prettier-js ()
   (use-package prettier-js
     :defer t
@@ -74,3 +54,43 @@ true."
                                "--bracket-spacing" "false"
                                "--no-semi"
                                "--single-quote")))))
+
+(defun aj-javascript/post-init-add-node-modules-path ()
+  (with-eval-after-load 'rjsx-mode
+    (add-hook 'rjsx-mode-hook #'add-node-modules-path)))
+
+(defun aj-javascript/post-init-company ()
+  (spacemacs|add-company-hook rjsx-mode))
+
+(defun aj-javascript/post-init-company-flow ()
+  (spacemacs|add-company-backends
+    :backends
+    '((company-flow :with company-dabbrev-code)
+      company-files)))
+
+;; (defun aj-javascript/post-init-company-tern ()
+;;   (push 'company-tern company-backends-rjsx-mode))
+
+(defun aj-javascript/post-init-evil-matchit ()
+  (with-eval-after-load 'evil-matchit
+    (plist-put evilmi-plugins 'rjsx-mode
+               '((evilmi-simple-get-tag evilmi-simple-jump)
+                 (evilmi-javascript-get-tag evilmi-javascript-jump)
+                 (evilmi-html-get-tag evilmi-html-jump)))))
+
+(defun aj-javascript/post-init-flycheck ()
+  (with-eval-after-load 'flycheck
+    (push 'javascript-jshint flycheck-disabled-checkers)
+    (push 'json-jsonlint flycheck-disabled-checkers)
+    (flycheck-add-mode 'javascript-eslint 'rjsx-mode))
+  (add-hook 'rjsx-mode-hook #'spacemacs//react-use-eslint-from-node-modules)
+  (spacemacs/add-flycheck-hook 'rjsx-mode))
+
+(defun aj-javascript/post-init-smartparens ()
+  (if dotspacemacs-smartparens-strict-mode
+      (add-hook 'rjsx-mode-hook #'smartparens-strict-mode)
+    (add-hook 'rjsx-mode-hook #'smartparens-mode)))
+
+;; (defun aj-javascript/post-init-tern ()
+;;   (add-hook 'rjsx-mode-hook 'tern-mode)
+;;   (spacemacs//set-tern-key-bindings 'rjsx-mode))
