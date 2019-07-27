bf3f62d69168a0d6f3f8aeb488dca8513139d9d4
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-27 20:45:36 +0200

    remove old function

--- layers/aj-javascript/funcs.el
+++ layers/aj-javascript/funcs.el
@@ -11,15 +11,3 @@
     (beginning-of-line)
     (when (looking-at-p "^ +\/?> *$")
       (delete-char sgml-basic-offset))))
-
-(defun spacemacs//react-use-eslint-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (global-eslint (executable-find "eslint"))
-         (local-eslint (expand-file-name "node_modules/.bin/eslint"
-                                         root))
-         (eslint (if (file-executable-p local-eslint)
-                     local-eslint
-                   global-eslint)))
-    (setq-local flycheck-javascript-eslint-executable eslint)))
