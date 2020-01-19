73d3750bd3f18c0d6a7a6c4ec609d19bf4d5d43f
Mario Fernandez <mario.fernandez@hceris.com>
2020-01-19 12:32:49 +0100

    switch to jetbrains mono

--- init.el
+++ init.el
@@ -158,8 +158,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Inconsolata"
-                               :size 18
+   dotspacemacs-default-font '("JetBrains Mono"
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
