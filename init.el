46d84026310a339519312c4eeb39ab133311ef07
Mario Fernandez <mario.fernandez@hceris.com>
2020-07-01 18:53:30 +0200

    ignore token like variables for env vars

--- init.el
+++ init.el
@@ -483,6 +483,7 @@ This function defines the environment variables for your Emacs session. By
 default it calls `spacemacs/load-spacemacs-env' which loads the environment
 variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
 See the header of this file for more information."
+  (setq spacemacs-ignored-environment-variables '(".*_TOKEN"))
   (spacemacs/load-spacemacs-env)
   )
 
