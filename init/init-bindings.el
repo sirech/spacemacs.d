01a9bdf4bf6e57ec6b8ddc7923338d641818e882
Mario Fernandez <mario.fernandez@hceris.com>
2024-11-01 15:34:13 +0100

    fix control tab override (hacky)

--- init/init-bindings.el
+++ init/init-bindings.el
@@ -1,4 +1,16 @@
 (define-key evil-insert-state-map (kbd "C-d") 'delete-char)
-(define-key evil-normal-state-map (kbd "<C-tab>") 'spacemacs/alternate-buffer)
+
+;; Not exactly sure which one of this does actually the trick, helm seems to overwrite this
+(define-key evil-normal-state-map (kbd "C-<tab>") 'spacemacs/alternate-buffer)
+(define-key evil-motion-state-map (kbd "<C-tab>") 'spacemacs/alternate-buffer)
+(map! (:after evil
+              :m (kbd "<C-tab>") 'spacemacs/alternate-buffer))
+(map! (:after helm
+              :m (kbd "<C-tab>") 'spacemacs/alternate-buffer))
+(with-eval-after-load 'helm-mode
+  '(progn
+     (define-key evil-normal-state-map (kbd "C-<tab>") 'spacemacs/alternate-buffer)
+     (define-key evil-motion-state-map (kbd "<C-tab>") 'spacemacs/alternate-buffer)
+     ))
 
 (provide 'init-bindings)
